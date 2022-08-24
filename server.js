fdd1720b76dccc62934d0eb25ca8ad8ed4119609
kamal kishore <kamal.kishore981@gmail.com>
2022-08-24 14:42:19 -0400

    add kamal heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 // Sets up the Express App
 const app = express();
 // Sets port for listening and let heroku decide on port, if not, use port 8080
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 80;
 
 //serve images, CSS files, and JavaScript files in a directory named public
 app.use(express.static("public"));
